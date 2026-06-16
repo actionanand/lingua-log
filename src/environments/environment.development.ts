fd5b3264a5b5a25e56233bdcd0c96573b323901d
Anand raja <46064269+actionanand@users.noreply.github.com>
2026-06-16 16:47:23 +0000

    feat: initialize environment configuration for development and production

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,6 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  GOOGLE_SHEET_ID: '1OZZ3XTy1OglXUrHOfmtpMMd9tDqyMlx4OJ8TLnKk310', // Replace with actual Sheet ID from URL
+  SHEET_GID: 992413678, // Sheet tab number (0 for first tab)
+  passwordHash: '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', // Default hash for 'password'
+};
